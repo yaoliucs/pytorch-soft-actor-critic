adc5e0f01bfafaf6d2451b313291d2ddd2e527c9
yaoliucs <yao.liu.chn@gmail.com>
2019-12-16 17:40:45 -0800

    online exp

--- main.py
+++ main.py
@@ -40,10 +40,10 @@ parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')
 parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
                     help='model updates per simulator step (default: 1)')
-parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
-                    help='Steps sampling random actions (default: 10000)')
-parser.add_argument('--end_steps', type=int, default=10000, metavar='N',
-                    help='Total steps sampling actions (default: 10000)')
+parser.add_argument('--start_steps', type=int, default=0, metavar='N',
+                    help='Steps sampling random actions (default: 0)')
+parser.add_argument('--end_steps', type=int, default=1000001, metavar='N',
+                    help='Total steps sampling actions (default: 1000001)')
 parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
                     help='Value target update per no. of updates per step (default: 1)')
 parser.add_argument('--replay_size', type=int, default=10000, metavar='N',
